2bd0e1f3c8cc8c9954fb2137162834ae31543938
andrearalevska <120467890+andrearalevska@users.noreply.github.com>
2023-03-07 02:36:20 +0100

    Add files via upload

---
+++ Homework 2/StudentGroup/Program.cs
@@ -0,0 +1,28 @@
+﻿string[] studentsG1 = new string[] { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+string[] studentsG2 = new string[] { "Ana", "Elena", "Ivana", "Marija", "Simona" };
+
+Console.WriteLine("Enter student group ( there are 1 and 2 ):");
+string studentsGroup = Console.ReadLine();
+
+if(studentsGroup == "1")
+{
+    Console.WriteLine("Students in Group 1 are:");
+    
+    foreach (string student in studentsG1)
+    {
+        Console.WriteLine(student);
+    }
+}
+else if (studentsGroup == "2")
+{
+    Console.WriteLine("Students in Group 2 are:");
+    
+    foreach (string student in studentsG2)
+    {
+        Console.WriteLine(student);
+    }
+}
+else
+{
+    Console.WriteLine("Please choose 1 or 2!");
+}
---
+++ Homework 2/SumOfEven/Program.cs
@@ -0,0 +1,60 @@
+﻿Console.WriteLine("Enter first integer:");
+string firstInput = Console.ReadLine();
+
+int firstNumber;
+bool firstSuccess = int.TryParse(firstInput, out firstNumber);
+
+Console.WriteLine("Enter second integer:");
+string secondInput = Console.ReadLine();
+
+int secondNumber;
+bool secondSuccess = int.TryParse(secondInput, out secondNumber);
+
+Console.WriteLine("Enter third integer:");
+string thirdInput = Console.ReadLine();
+
+int thirdNumber;
+bool thirdSuccess = int.TryParse(thirdInput, out thirdNumber);
+
+Console.WriteLine("Enter fourth integer:");
+string fourthInput = Console.ReadLine();
+
+int fourthNumber;
+bool fourthSuccess = int.TryParse(fourthInput, out fourthNumber);
+
+Console.WriteLine("Enter fifth integer:");
+string fifthInput = Console.ReadLine();
+
+int fifthNumber;
+bool fifthSuccess = int.TryParse(fifthInput, out fifthNumber);
+
+Console.WriteLine("Enter sixth integer:");
+string sixthInput = Console.ReadLine();
+
+int sixthNumber;
+bool sixthSuccess = int.TryParse(sixthInput, out sixthNumber);
+
+
+if (firstSuccess == true && secondSuccess == true && thirdSuccess == true && fourthSuccess == true && fifthSuccess == true && sixthSuccess == true)
+{
+    int[] arrayOfIntegers = new int[] { firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber, sixthNumber };
+
+    int sum = 0;
+
+    for (int i = 0; i < arrayOfIntegers.Length; i++)
+    {
+        if (arrayOfIntegers[i] % 2 == 0)
+        {
+            sum += arrayOfIntegers[i];
+        }
+    }
+
+    Console.WriteLine("The sum of the even integers in this array is: " + sum);
+}
+
+else
+{
+    Console.WriteLine("Please enter only integer!");
+}
+
+
